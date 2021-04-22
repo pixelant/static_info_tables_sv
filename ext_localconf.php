04cb62fe5912ce6174cfe04bf354df4d19e9550b
Ihor Kulmatytskyy <ihor.kulmatytskyy@resultify.se>
2021-04-22 15:56:41 +0300

    [TASK] add compatibility with TYPO3v10

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,13 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/Extbase/setup.txt">'
-);
+call_user_func(function()
+{
+    $extensionKey = 'static_info_tables_sv';
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        $extensionKey,
+        'setup',
+        '@import \'EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.txt\''
+    );
+});
