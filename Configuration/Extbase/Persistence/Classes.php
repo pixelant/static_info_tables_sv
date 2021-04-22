cd47164169010ecddc267a925ff40838ee1f43c7
Ihor Kulmatytskyy <ihor.kulmatytskyy@resultify.se>
2021-04-22 04:15:26 +0300

    [TASK] updated versions of requirements, added Configuration/Extbase/Persistance/Classes.php

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    \SJBR\StaticInfoTables\Domain\Model\Country::class => [
+        'tableName' => 'static_countries',
+        'properties' => [
+            'shortNameSv' => [
+                'fieldName' => 'cn_short_sv',
+            ]
+        ],
+    ],
+];
--- ext_emconf.php
+++ ext_emconf.php
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'static_info_tables' => '6.4.0-6.5.99',
-            'typo3' => '7.6.0-8.99.99',
+            'static_info_tables' => '^6.4.0',
+            'typo3' => '7.6.0-10.99.99',
         ],
         'conflicts' => [
         ],
